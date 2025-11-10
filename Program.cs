97eb2d7af1f4bc15e46e7053f42553229c9eab8d
CARLOS BINDA <carlos-henrique.filho@caixa.gov.br>
2025-11-10 19:41:34 -0300

    Aula 01.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Teste;
+
+public class Program
+{
+    public static void Main()
+    {
+        CalcularPar();
+    }
+
+    private static void CalcularPar()
+    {
+        Console.WriteLine("Digita um Numero");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        int resultado = numero % 2;
+
+        if (resultado == 0)
+        {
+            Console.Write("O numero é par: " + numero);
+        }
+        else
+            Console.Write("O número é impar: " + numero);
+    }
+}
