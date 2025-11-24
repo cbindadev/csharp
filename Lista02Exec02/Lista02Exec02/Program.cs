4711d13f08e4f25d75a23cbc0157f724281f4a67
CARLOS BINDA <carlos-henrique.filho@caixa.gov.br>
2025-11-24 14:49:25 -0300

    Lista exercicios

---
+++ Lista02Exec02/Lista02Exec02/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] nums = {-7, -3, 2, 3, 11};
+
+
+int[] quadrados = new int[nums.Length];
+
+for (int i = 0; i < nums.Length; i++)
+{
+    quadrados[i] = nums[i] * nums[i];
+}
+
+Array.Sort(quadrados);
+
+Console.WriteLine("Array de quadrados ordenado:");
+foreach (int num in quadrados)
+{
+    Console.Write(num + " ");
+}
