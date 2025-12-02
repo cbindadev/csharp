768eb11f7cbfd6994e2fe97472d515cac24fec0b
CARLOS BINDA <carlos-henrique.filho@caixa.gov.br>
2025-12-01 21:16:11 -0300

    exercicios faltantes

---
+++ Lista02Exec04/Lista02Exec04/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.Write("Digite uma palavra: ");
+string palavra = Console.ReadLine();
+
+
+char[] s = palavra.ToCharArray();
+
+for (int i = 0; i < s.Length / 2; i++)
+{
+    char temp = s[i];
+    s[i] = s[s.Length - 1 - i];
+    s[s.Length - 1 - i] = temp;
+}
+
+Console.WriteLine("Invertida: " + new string(s));
